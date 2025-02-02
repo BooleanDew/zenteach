e60b25b86e0a313b8a122acd4fd697f049e1df6e
BooleanDew <booleandew@gmail.com>
2025-02-02 20:49:28 -0300

    g

--- zenteach/wsgi.py
+++ zenteach/wsgi.py
@@ -1,6 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
-
+from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zenteach.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root='/opt/render/project/src/staticfiles')
