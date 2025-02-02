5554dd3a6f50066f94daa742b685d0dff4dedb65
BooleanDew <booleandew@gmail.com>
2025-02-02 15:45:29 -0300

    D

--- zenteach/settings.py
+++ zenteach/settings.py
@@ -72,7 +72,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(str(BASE_DIR), 'staticfiles')
 #STATIC_ROOT = os.environ.get('STATIC_ROOT', os.path.join(BASE_DIR, 'staticfiles'))
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(str(BASE_DIR), 'static')
 ]
 
 # Media Files
